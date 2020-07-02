692f03872e076b5abf1e05faa3ffa2c2cab34223
Rudi Farkas <rudi.farkas@gmail.com>
2020-07-02 21:25:18 +0200

    add makeLayout code

--- NSCollectionViewCompositionalLayoutDemo/ViewController.swift
+++ NSCollectionViewCompositionalLayoutDemo/ViewController.swift
@@ -9,36 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     private var collectionView: UICollectionView!
-    
+
     private var sectionColors: [UIColor] = [
         .systemRed,
         .systemBlue,
         .systemGreen,
         .systemTeal,
         .systemYellow,
-        .systemGray
+        .systemGray,
     ]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         makeCollectionView()
     }
-    
+
     private func makeCollectionView() {
-        collectionView = UICollectionView.init(frame: .zero,
-                                               collectionViewLayout: makeLayout())
+        collectionView = UICollectionView(frame: .zero,
+                                          collectionViewLayout: makeLayout())
         collectionView.backgroundColor = .orange
         collectionView.dataSource = self
         view.addSubview(collectionView)
-        
+
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        
+
         collectionView.register(CollectionViewCell.self,
                                 forCellWithReuseIdentifier: "CollectionViewCell")
         collectionView.register(BadgeSupplementaryView.self,
@@ -48,32 +47,50 @@ class ViewController: UIViewController {
                                 forSupplementaryViewOfKind: "header",
                                 withReuseIdentifier: "HeaderSupplementaryView")
     }
-    
+
     private func makeLayout() -> UICollectionViewLayout {
-        return UICollectionViewLayout()
+        // 1
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .absolute(50))
+
+        // 2
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        // 3
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: itemSize,
+                                                       subitems: [item])
+
+        // 4
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 5
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                        leading: 5,
+                                                        bottom: 5,
+                                                        trailing: 5)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
-    
 }
 
 extension ViewController: UICollectionViewDataSource {
-    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 2
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 16
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
-        
+
         cell.label.text = indexPath.section.description + " - " + indexPath.item.description
         cell.backgroundColor = sectionColors[indexPath.section]
-        
+
         return cell
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case "header":
@@ -81,14 +98,14 @@ extension ViewController: UICollectionViewDataSource {
                                                                              withReuseIdentifier: "HeaderSupplementaryView",
                                                                              for: indexPath) as! HeaderSupplementaryView
             headerView.label.text = "This is a header"
-            
+
             return headerView
         case "badge":
             let badgeView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
                                                                             withReuseIdentifier: "BadgeSupplementaryView",
                                                                             for: indexPath) as! BadgeSupplementaryView
             badgeView.label.text = indexPath.section.description
-            
+
             return badgeView
         default:
             print(kind)
